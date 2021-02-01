b1117511187b5868bc1069a7c2f6b26dcd33e0cd
bsh <dan990429@gmail.com>
2021-02-01 22:42:14 +0000

    t

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import { getInput, setOutput, setFailed } from '@actions/core'
-import { context } from '@actions/github'
+const core = require('@actions/core')
+const github = require('@actions/github')
 
 
 try {
-    const nameToGreet = getInput('whogreet')
+    const nameToGreet = core.getInput('whogreet')
     console.log(`hello ${nameToGreet}`)
 
     const time = (new Date()).toString()
-    setOutput('time', time)
+    core.setOutput('time', time)
 
-    const payload = JSON.stringify(context.payload, undefined, 2)
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event playload ${payload}`)
 } catch (error) {
-    setFailed(error.message)
+    core.setFailed(error.message)
 }
